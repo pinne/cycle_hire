6c8f3ed18f4cd1bec9373bf1de629e5a6f48d03a
Simon Kers <skers@kth.se>
2013-02-18 21:57:30 +0100

    Supervisor works as intended

---
+++ src/ev_supervisor.erl
@@ -0,0 +1,76 @@
+%%%===================================================================
+%%% The Erlangville Cycle Hire System
+%%% Simon Kers -- KTH 2013
+%%%===================================================================
+-module(ev_supervisor).
+-author('skers@kth.se').
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0,
+         start_child/2]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-record(state, {ref, pid, total, occupied}).
+
+-define(DBNAME, ev_db).
+-define(CHILD(Id, Mod, Type, Args), {Id, {Mod, start_link, Args},
+                                     permanent, 5000, Type, [Mod]}).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    {ok, Pid} = supervisor:start_link({local, ?MODULE}, ?MODULE, []),
+    erlang:unlink(Pid),
+    {ok, Pid}.
+
+start_child(Total, Occupied) ->
+    %% create a unique reference for the station
+    StationRef = make_ref(),
+    Args = [StationRef, Total, Occupied],
+    {ok, _Pid} = supervisor:start_child(?MODULE, Args),
+    {ok, StationRef}.
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%%
+%% @spec init(Args) -> {ok, {SupFlags, [ChildSpec]}} |
+%%                     ignore |
+%%                     {error, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init(Args) ->
+    %% Create global ets, where every docking station records its state
+    ets:new(ev_db, [set, public, named_table, {keypos, #state.ref}]),
+
+    Restart = {simple_one_for_one, 5, 10},
+    ChildSpec = {station,
+                 {ev_docking_station, start_link, Args},
+                 permanent, 5000, worker,
+                 [ev_docking_station]},
+    {ok, {Restart, [ChildSpec]}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
